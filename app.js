d571a78b9a5b9b82c5857cc5f549044c7167f759
Carina <cillgen@cgs-online.de>
2023-10-20 11:46:56 +0200

    hide start button and show first vocab card

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 class App {
-  async onClick() {
+  async startTraining() {
+    this.hideElement("#startButton");
+    await this.loadVocabulary();
+    this.updateCard();
+    this.showElement("#card");
+  }
+
+  showElement(selector) {
+    const element = document.querySelector(selector);
+    element.classList.remove("hidden");
+  }
+
+  hideElement(selector) {
+    const element = document.querySelector(selector);
+    element.classList.add("hidden");
+  }
+
+  async loadVocabulary() {
     const msg = await fetch("vocabulary.json");
-    const json = await msg.json();
+    this.json = await msg.json();
+  }
+
+  updateCard() {
+    document.querySelector("#card h2").innerText = this.json.vocabulary[0].es;
   }
 }
-const app = new App;
+
+const app = new App();
